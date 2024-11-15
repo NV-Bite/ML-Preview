27cc67180d39d83e90cc2b3f3a977044cf7ef53e
rizkyyanuark <rizkyyanuarkristianto@gmail.com>
2024-11-15 11:02:16 +0700

    final deploy

--- func.py
+++ func.py
@@ -3,46 +3,33 @@ import os
 import io
 from dotenv import load_dotenv
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import numpy as np
+
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
 
 
-# Load model
-MODEL_PATH = "model/Densenet_model.h5"  # Ganti dengan path model Anda
-model = tf.keras.models.load_model(MODEL_PATH)
+@st.cache_data
+def default():
+    response = requests.get(API_URL)
 
-
-def load_and_preprocess_image(img_path, target_size=(224, 224)):
-    img = load_img(img_path, target_size=target_size)
-    img_array = img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-    img_array = img_array / 255.0  # Rescale image
-    return img_array
-
-
-def predict_image(model, img_array, class_labels):
-    predictions = model.predict(img_array)
-    predicted_class_index = np.argmax(predictions)
-    predicted_class_label = class_labels[predicted_class_index]
-    return predicted_class_label, predictions[0][predicted_class_index]
+    if response.status_code == 200:
+        # data = response.json()
+        return response.text
+    else:
+        data = "An error occurred:", response.status_code
+        return data
 
 
 @st.cache_data
 def predict(image):
-    # Save the uploaded image to a temporary file
-    img_path = "temp_image.jpg"
-    with open(img_path, "wb") as f:
-        f.write(image)
+    url = API_URL + "/predict/"
 
-    # Preprocess the image
-    img_array = load_and_preprocess_image(img_path)
+    payload = {}
+    files = {"file": ("image.jpg", io.BytesIO(image), "image/jpeg")}
+    headers = {}
 
-    # Define class labels
-    # Ganti dengan label kelas Anda
-    class_labels = ['nasi padang', 'pizza', 'soto ayam']
+    response = requests.request(
+        "POST", url, headers=headers, data=payload, files=files)
 
-    # Predict using the model
-    predicted_class, confidence = predict_image(model, img_array, class_labels)
-
-    return {"class": predicted_class, "confidence": confidence}
+    return response.text
